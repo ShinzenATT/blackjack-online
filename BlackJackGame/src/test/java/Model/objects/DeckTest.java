b040938ec91e9188df15ea9c684236900d0c2c50
lesonion <blomgren.andin@hotmail.com>
2022-02-28 16:42:04 +0100

    Deck testing added

--- BlackJackGame/src/test/java/Model/objects/DeckTest.java
+++ BlackJackGame/src/test/java/Model/objects/DeckTest.java
@@ -2,24 +2,39 @@ package Model.objects;
 
 import org.junit.Test;
 
+import java.util.Collections;
+import java.util.List;
+import java.util.stream.Collectors;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotEquals;
+
+
 public class DeckTest {
 
 
     @Test
-    public void deckMethod(){
-        //First test one unshuffled deck
-        try{
-            Deck one = new Deck(1, false);
-            System.out.println("One unshuffled deck has " + one.getNumCards() + "cards.");
-        }catch(Exception e){
-            System.out.println("One unshuffled exception: " + e);
-        }
+    //Suffled
+    public void shuffledDeck(){
+        //Lätt shuffle testing
+        Deck firstShuffled = new Deck(1, true);
+        Deck secondShuffled = new Deck(1, true);
+        assertNotEquals(firstShuffled, secondShuffled);
+    }
 
-        try{
-            Deck two = new Deck(6, true);
-            System.out.println("Six shuffled deck has " + two.getNumCards() + "cards.");
-        }catch(Exception e){
-            System.out.println("6 shuffled exception: " + e);
-        }
+    @Test
+    //no duplicates
+    public void noDouplicates(){
+        Deck deck = new Deck(1, false);
+        List deckList = deck.getDeck();
+        assertEquals(0, deckList.stream().filter(c -> Collections.frequency(deckList, c) > 1).toList().size());
+    }
+    @Test
+    //drawing card reduces size of remaining deck
+    public void sizeOfDeck(){
+        Deck deck = new Deck(1, false);
+        int size = deck.getNumCards();
+        deck.next();
+        assertNotEquals(size, deck.getNumCards());
     }
 }
