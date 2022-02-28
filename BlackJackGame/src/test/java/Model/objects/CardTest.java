f77b57e725143f36c73d47c82bbad45a1edd9bbe
LesOnion <blomgren.andin@hotmail.com>
2022-02-28 02:02:38 +0100

    Basic what I think is test classes

--- BlackJackGame/src/test/java/Model/objects/CardTest.java
+++ BlackJackGame/src/test/java/Model/objects/CardTest.java
@@ -1,4 +1,17 @@
 package Model.objects;
 
+import org.junit.Test;
+
+
 public class CardTest {
+
+
+    @Test
+    public void cardMethod(){
+        for (Card.Suit suit : Card.Suit.values()) {
+            for (Card.Rank rank : Card.Rank.values()) {
+                System.out.println(new Card(rank,suit));
+            }
+        }
+    }
 }
--- BlackJackGame/src/test/java/Model/objects/DeckTest.java
+++ BlackJackGame/src/test/java/Model/objects/DeckTest.java
@@ -1,4 +1,25 @@
 package Model.objects;
 
+import org.junit.Test;
+
 public class DeckTest {
+
+
+    @Test
+    public void deckMethod(){
+        //First test one unshuffled deck
+        try{
+            Deck one = new Deck(1, false);
+            System.out.println("One unshuffled deck has " + one.getNumCards() + "cards.");
+        }catch(Exception e){
+            System.out.println("One unshuffled exception: " + e);
+        }
+
+        try{
+            Deck two = new Deck(6, true);
+            System.out.println("Six shuffled deck has " + two.getNumCards() + "cards.");
+        }catch(Exception e){
+            System.out.println("6 shuffled exception: " + e);
+        }
+    }
 }
