791871ca525a899a92ab71c2499e2bb79ba11cec
lesonion <blomgren.andin@hotmail.com>
2022-02-28 15:22:47 +0100

    Actual good card testing

--- BlackJackGame/src/test/java/Model/objects/CardTest.java
+++ BlackJackGame/src/test/java/Model/objects/CardTest.java
@@ -2,16 +2,15 @@ package Model.objects;
 
 import org.junit.Test;
 
+import static org.junit.Assert.*;
+
 
 public class CardTest {
 
-
     @Test
-    public void cardMethod(){
-        for (Card.Suit suit : Card.Suit.values()) {
-            for (Card.Rank rank : Card.Rank.values()) {
-                System.out.println(new Card(rank,suit));
-            }
-        }
+    public void createCard(){
+        Card queenOfSpades = new Card(Card.Rank.QUEEN, Card.Suit.SPADES);
+        assertEquals(Card.Rank.QUEEN, queenOfSpades.getRank());
+        assertEquals(Card.Suit.SPADES, queenOfSpades.getSuit());
     }
 }
