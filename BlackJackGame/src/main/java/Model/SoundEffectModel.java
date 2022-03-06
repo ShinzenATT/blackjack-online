dbc89bf36bc6b34052668523a903c658132badda
jopers <joel.persson.p@gmail.com>
2022-03-06 14:26:55 +0100

    javadoc added SoundEffectModel.java

--- BlackJackGame/src/main/java/Model/SoundEffectModel.java
+++ BlackJackGame/src/main/java/Model/SoundEffectModel.java
@@ -6,8 +6,18 @@ import javax.sound.sampled.Clip;
 import java.io.File;
 import java.util.concurrent.CompletableFuture;
 
+/**
+ * Class that tries to play sound files.
+ */
 public class SoundEffectModel {
-    
+  private Clip bgClip;
+
+  /** 
+   * Plays a supported sound file from the resources
+   * folder given the name of the file.
+   * 
+   * @param url The full name of the sound file to play.
+   */
   public static synchronized void playSound(final String url) {
     CompletableFuture.runAsync(() -> {
       try {
@@ -23,8 +33,12 @@ public class SoundEffectModel {
     });
   }
 
-  private Clip bgClip;
-
+  /** 
+   * Plays a supported sound file on repeat as background music
+   * from the resources folder given the name of the file.
+   * 
+   * @param url The full name of the sound file to play on repeat as background music.
+   */
   public synchronized void playBackgroundSound(final String url) {
     CompletableFuture.runAsync(() -> {
         try {
