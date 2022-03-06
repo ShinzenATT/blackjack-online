91e45c2c7f8c293348d5a4b757c72aec977556d9
jopers <joel.persson.p@gmail.com>
2022-03-06 14:16:11 +0100

    javadoc added ImageDisplayModel.java

--- BlackJackGame/src/main/java/Model/ImageDisplayModel.java
+++ BlackJackGame/src/main/java/Model/ImageDisplayModel.java
@@ -6,21 +6,26 @@ import java.util.Map;
 import java.awt.Image;
 import java.io.File;
 import java.io.IOException;
-
 import javax.imageio.ImageIO;
 import Model.objects.*;
 import Model.objects.Card.Rank;
 
+/**
+ * Class that tries to read custom images for all the playing cards, 
+ * linking images to set names in a hashmap making it easier to use them later.
+ */
 public class ImageDisplayModel {
     static private Map<String, Image> IMGS = new HashMap<String, Image>();
 
+    /**
+     * Creates a ImageDisplayModel object where all custom images for
+     * the playing cards are read and linked to a specific given name in a hashmap. 
+     */ 
     public ImageDisplayModel() {
         HashMap<String, Image> imgs = new HashMap<String, Image>();
-
         String dir = "BlackJackGame/src/main/resources/";
         String png = ".png";
     
-
         for (String suit = "diamonds", name; ;) {
             for (Rank rank : Rank.values()) {
                 name = (rank + "_of_" + suit).toLowerCase();
@@ -57,10 +62,26 @@ public class ImageDisplayModel {
         IMGS = Collections.unmodifiableMap(imgs);
     }
 
+    /** 
+     * Given the name linked with an read image, return the image from the hashmap.
+     * 
+     * @param name The specificly given name that associate with a read image.
+     * @return The image that was linked with given name.
+     */
     public Image getImageFromName(String name){
         return IMGS.get(name.toLowerCase());
     }
 
+    /** 
+     * 
+     * Given the name linked with an read image, scale it according
+     * to given width and height before returning the image.
+     * 
+     * @param name The specificly given name that associate with a read image.
+     * @param labelWidth The wanted width of the image.
+     * @param labelHeight The wanted height of the image.
+     * @return The scaled image that was linked with given name.
+     */
     public Image getScaledImageInstanceFromName(String name, int labelWidth, int labelHeight){
         return getImageFromName(name).getScaledInstance(labelWidth, labelHeight, Image.SCALE_SMOOTH);
     }
