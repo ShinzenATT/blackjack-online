c902f78ec5d828d65178b8681eb77cce5ef86441
lesonion <blomgren.andin@hotmail.com>
2022-03-07 16:02:48 +0100

    Javadoc documentation

--- BlackJack-Server/src/test/java/model/GameTrackerTest.java
+++ BlackJack-Server/src/test/java/model/GameTrackerTest.java
@@ -13,8 +13,11 @@ import static org.junit.Assert.*;
 public class GameTrackerTest {
 
 
+    /**
+     * Testing that creates two players and starts a game, and then
+     * tests that there is a next player.
+     */
     @Test
-    //Testing for next player
     public void playerTest(){
         Player player1 = new Player("Player1", 100);
         Player player2 = new Player("Player2", 100);
@@ -22,8 +25,12 @@ public class GameTrackerTest {
         assertTrue(testGame.hasNext());
     }
 
+    /**
+     * Testing that starts a testGame and makes sure that the turn
+     * order is larger than one. This is because if there is one or fewer
+     * players in the game it's either only the dealer or no-one playing.
+     */
     @Test
-    //Testing for a turn order
     public void nextTurnTest(){
         Player player1 = new Player("Player1", 100);
         Player player2 = new Player("Player2", 100);
@@ -31,9 +38,11 @@ public class GameTrackerTest {
         assertTrue(testGame.getTurnOrder().size() > 1);
     }
 
-
+    /**
+     * Testing that whenever we use the method to fetch a next card from
+     * the deck, it fetches a card and not something else.
+     */
     @Test
-    //Testing for next card (fixa så att den kollar Card och inte had)
     public void cardTest(){
         Player player1 = new Player("Player1", 100);
         Player player2 = new Player("Player2", 100);
@@ -50,6 +59,11 @@ public class GameTrackerTest {
         assertEquals("Dealer", testGame.getDealerHand().getPlayer().getUsername());
     }*/
 
+    /**
+     * Test for fully functional turn order handling. Creating a test game
+     * then assigning players to positions and comparing them to the return
+     * value from the getter.
+     */
     @Test
     //Testing for getting the correct turn order
     public void turnOrderTest(){
