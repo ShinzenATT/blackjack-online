35288756c0fa67c9f21105d5222b7108e5383941
Joel <47850057+jopers@users.noreply.github.com>
2022-03-07 23:38:28 +0100

    added javadoc

--- BlackJack-Server/src/main/java/json_data/GameModel.java
+++ BlackJack-Server/src/main/java/json_data/GameModel.java
@@ -7,12 +7,24 @@ import model.objects.Hand;
 import java.util.ArrayList;
 import java.util.List;
 
+/**
+ * Class that holds data for a game.
+ * @author Alen
+ * @version 2022-03-07
+ */
 public class GameModel {
     public final String status;
     public final int current_turn;
     public final String room_code;
     public final List<Hand> turn_order;
 
+    /**
+     * Creates a GameModel object that keeps
+     * data about a game.
+     * 
+     * @param gt GameTracker to follow.
+     * @param roomCode The room code of the room.
+     */
     public GameModel(GameTracker gt, String roomCode) {
         current_turn = gt.getTurnTracker() - 1;
         turn_order = gt.getTurnOrder();
@@ -27,6 +39,13 @@ public class GameModel {
         }
     }
 
+    /**
+     * Creates a GameModel that keeps
+     * data about a game.
+     * 
+     * @param gcList List of game connections.
+     * @param roomCode The room code of the room.
+     */
     public GameModel(List<GameConnection> gcList, String roomCode){
         status = "waiting";
         current_turn = 0;
--- BlackJack-Server/src/main/java/json_data/RecievingCmd.java
+++ BlackJack-Server/src/main/java/json_data/RecievingCmd.java
@@ -1,14 +1,27 @@
 package json_data;
 
+/**
+ * Class that handles commands to the server.
+ * @author Alen
+ * @version 2022-03-07
+ */
 public class RecievingCmd {
     public String command;
     public int value;
 
+    /**
+     * Creates a command object with given command and value.
+     * @param c The command.
+     * @param v The value.
+     */
     public RecievingCmd(String c, int v){
         command = c;
         value = v;
     }
 
+    /**
+     * Converts the data from a command object to a single string.
+     */
     @Override
     public String toString() {
         return "RecievingCmd{" +
